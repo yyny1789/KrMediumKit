4535d6292b4712c9217ba0b9c6518c5483e26743
yangyang <yangyang02@36kr.com>
2017-05-18 15:46:47 +0800

    initialize

---
+++ KrMediumKit/NetworkManager/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  KrMediumKit
+//
+//  Created by 杨洋 on 2017/5/18.
+//  Copyright © 2017年 com.36kr. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func testLog() {
+        print("networkManager testLog")
+    }
+    
+}
