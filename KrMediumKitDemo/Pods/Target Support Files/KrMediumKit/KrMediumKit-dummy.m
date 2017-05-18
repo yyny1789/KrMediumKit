f8edb9ea1784b5ee8678d50275ec57e0a1ea6aba
yangyang <yangyang02@36kr.com>
2017-05-18 20:06:39 +0800

    update

--- KrMediumKitDemo/Pods/Target Support Files/KrMediumKit/KrMediumKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KrMediumKit : NSObject
-@end
-@implementation PodsDummy_KrMediumKit
-@end
--- KrMediumKitDemo/Pods/Target Support Files/Pods-KrMediumKitDemo/Pods-KrMediumKitDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KrMediumKitDemo : NSObject
-@end
-@implementation PodsDummy_Pods_KrMediumKitDemo
-@end
